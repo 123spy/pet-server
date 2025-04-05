fcff5f136c159752fed99aece32d7f6b8a5373a8
123spy <1285230617@qq.com>
2025-04-05 11:47:17 +0800

    update

--- config/config.default.js
+++ config/config.default.js
@@ -1,5 +1,5 @@
 module.exports = {
-    dbURL: "mongodb://localhost:27017/pet_admin",
+    dbURL: "mongodb+srv://lansi:abcd1234@cluster0.cd0v4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/pet_admin",
     SecretId: 'AKIDpfNuYGD8KasNwjgGpJk5WzZ6lbQUhuHm',
     SecretKey: 'hXORbpJ518LTbglmhlpkYUPGESuKgxAa',
     Bucket: 'management-1306526770',
